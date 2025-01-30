7b2a820079204b88b1a3200487b6b5fef3a2280b
DanielAndrade53 <danielftandrade53@gmail.com>
2025-01-30 13:04:44 +0000

    Add src/components/ScreenOn

---
+++ src/components/ScreenOn.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import './ScreenOn.css';
+import ClockApp from './ClockApp';
+
+const ScreenOn = () => {
+    const [activeApp, setActiveApp] = useState(null);
+
+    const openApp = (appName) => {
+        setActiveApp(appName);
+    };
+
+    const closeApp = () => {
+        setActiveApp(null);
+    };
+
+    return (
+        <div className='screen-on'>
+
+            {activeApp === 'clock' ? (
+                <div className="full-screen-app">
+                    <ClockApp />
+                    <div className='bar' onClick={closeApp}></div>
+                </div>
+            ) : (
+                <div className='apps-container'>
+                    <div className='calendar-container'>{/* Calendar Here */}</div>
+
+                    <div className='app-icon'>App 1</div>
+                    <div className='app-icon'>App 2</div>
+                    <div className='app-icon'>App 3</div>
+                    <div className='app-icon'>App 4</div>
+                    
+                    <div className='weather-container'></div>
+
+                    <div className='app-icon'>App 5</div>
+                    <div className='app-icon'>App 6</div>
+                    <div className='app-icon'>App 7</div>
+                    
+                    <div className='app-icon' onClick={() => openApp('clock')}>
+                        <img className='icon' src='src/assets/clock-app.png' alt=''/>
+                    </div>
+
+                    <div className='app-icon'>App 9</div>
+                    <div className='app-icon'>App 10</div>
+                    <div className='app-icon'>App 11</div>
+                    <div className='app-icon'>App 12</div>
+
+                    <div className='search-container'>
+                        <search className='search'>Search</search>
+                    </div>
+
+                    <div className='footer-container'>
+                        <div className='app-icon'>App 13</div>
+                        <div className='app-icon'>App 14</div>
+                        <div className='app-icon'>App 15</div>
+                        <div className='app-icon'>App 16</div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ScreenOn;
