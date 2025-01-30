444ab58a356ac0ee36d3669e957c7a2a392cb163
DanielAndrade53 <danielftandrade53@gmail.com>
2025-01-30 13:00:02 +0000

    Add src/ files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect  } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Iphone from './components/Iphone'
+
+function App() {
+
+  return (
+    <>
+      <Iphone/>
+    </>
+  );
+}
+
+export default App;
