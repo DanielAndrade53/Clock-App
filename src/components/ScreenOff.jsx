c5852c43e46496d8a9afbbfe154b02e3c103333e
DanielAndrade53 <danielftandrade53@gmail.com>
2025-01-31 16:16:38 +0000

    Add src/components/ files

--- src/components/ScreenOff.jsx
+++ src/components/ScreenOff.jsx
@@ -7,14 +7,19 @@ const ScreenOff = () => {
 
     return (
         <div className='screen-off'>
+           
             <div className='day-time-container'>
-                <div className='day'>{day}</div>
-                <div className='time'>{time}</div>
+                <div className='day'>4</div>
+                <div className='timer'>5</div>
             </div>
 
-            <div className='icons'>
-                <div className='icon-1' />
-                <div className='icon-2'/>
+            <div className='bottom-icons'>
+                <div className='icon-flashlight'>
+                    <img src='' alt='' />
+                </div>
+                <div className='icon-camera'>
+                    <img src='' alt='' />
+                </div>
             </div>
         </div>
     );
